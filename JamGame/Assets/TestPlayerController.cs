f6281e3cb4f053f3023a357e0b25c25acfbf2798
Cinnazeyy <imcinnazeyy@gmail.com>
2022-02-22 20:54:11 +0100

    Renamed class name to solve overlap

--- JamGame/Assets/TestPlayerController.cs
+++ JamGame/Assets/TestPlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerController : MonoBehaviour
+public class TestPlayerController : MonoBehaviour
 {
     public Camera cam;
     public NavMeshAgent agent;
