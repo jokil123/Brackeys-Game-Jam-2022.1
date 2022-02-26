ac37fad6aec8796ada62e01ea5b65c4bdb852466
jo_kil <jo.kil.plays@gmail.com>
2022-02-26 20:22:21 +0100

    fixed player + stuff

---
+++ JamGame/Assets/Scripts/CheeseMode.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class CheeseMode : MonoBehaviour
+{
+    bool cheeseModeActive = false;
+
+    [SerializeField]
+    Material cheeseMaterial;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1) && !cheeseModeActive) {
+            MeshRenderer[] meshRenderers = GameObject.FindObjectsOfType<MeshRenderer>();
+
+            foreach (MeshRenderer meshRenderer in meshRenderers)
+            {
+                Debug.Log(meshRenderer.gameObject.transform.root.name.Contains("Player"));
+
+                if (meshRenderer.gameObject.transform.root.name.Contains("Player")) { continue; }
+
+                var mats = new Material[meshRenderer.materials.Length];
+
+                for (var j = 0; j < meshRenderer.materials.Length; j++)
+                {
+                    mats[j] = cheeseMaterial;
+                }
+
+                meshRenderer.materials = mats;
+            }
+            // cheeseModeActive = true;
+        }
+    }
+}
