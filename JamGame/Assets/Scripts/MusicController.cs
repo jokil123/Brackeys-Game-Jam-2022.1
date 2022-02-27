cdcd7f1d63674805e37fd2f111e39aeae5117cc8
BlackiTheDiamond <lorenzschwarzmann@gmail.com>
2022-02-27 11:31:18 +0100

    Music

--- JamGame/Assets/Scripts/MusicController.cs
+++ JamGame/Assets/Scripts/MusicController.cs
@@ -21,7 +21,7 @@ public class MusicController : MonoBehaviour
         {
             sources[intensitya].volume =sources[intensitya].volume+0.1f;
             sources[intensity].volume=sources[intensity].volume-0.1f;
-            yield return new WaitForSeconds(.1f);
+            yield return new WaitForSeconds(.3f);
         }
         intensity = intensitya;
     }
@@ -35,27 +35,19 @@ public class MusicController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Intro.volume = volume;
-        Intro.Play();
+        for (int i = 0; i < sources.Count; i++)
+        {
+            sources[i].volume = 0;
+            sources[i].Play();
+        }
+        sources[intensity].volume = volume;
+        IntroEnded = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IntroEnded == 0)
-        {
-            if (Intro.isPlaying == false)
-            {
-                for (int i = 0; i < sources.Count; i++)
-                {
-                    sources[i].volume = 0;
-                    sources[i].Play();
-                }
-                sources[intensity].volume = volume;
-                IntroEnded = 1;
-            }
-        }
-        
+      
     }
 
 }
